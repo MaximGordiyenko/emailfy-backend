3a597e310fd9160f8938bc7a26bc0a3b1abda5b9
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2025-03-14 19:54:45 +0200

    fix router object

--- service/ai/gemini.service.js
+++ service/ai/gemini.service.js
@@ -15,6 +15,7 @@ export const getGeminiResponse = async (message, modelType) => {
   try {
     const model = genAI.getGenerativeModel({ model: `${modelType}` });
     const result = await model.generateContent(message);
+    
     return result.response.text();
   } catch (error) {
     console.error("Gemini API Error:", error);
