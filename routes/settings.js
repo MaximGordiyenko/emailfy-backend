e262b9645a647299cac783c02e02e536fb4a58e0
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2025-03-19 02:17:27 +0200

    [EF-7]feat: routes with generates data for emails

--- routes/settings.js
+++ routes/settings.js
@@ -2,7 +2,15 @@ import express from 'express';
 import multer from 'multer';
 
 import { authenticateToken } from '../middleware/auth.middleware.js';
-import { uploadFile, getProfileImage, updateAccountInformation, getCountries } from '../controllers/settings.controller.js';
+import {
+  uploadFile,
+  getProfileImage,
+  updateAccountInformation,
+  getCountries,
+  generateClientEmails,
+  generateEmailCampaigns,
+  generateEmailsStatistic
+} from '../controllers/settings.controller.js';
 
 const upload = multer({ storage: multer.memoryStorage() }); // Use memory storage for multer
 
@@ -10,6 +18,9 @@ const router = express.Router();
 
 router.post('/settings/upload', upload.single('file'), uploadFile);
 router.post('/settings/update', authenticateToken, updateAccountInformation);
+router.post('/settings/user-info/client-emails', authenticateToken, generateClientEmails);
+router.post('/settings/user-info/client-campaigns', authenticateToken, generateEmailCampaigns);
+router.post('/settings/user-info/client-statistic', authenticateToken, generateEmailsStatistic);
 
 router.get('/settings/images', getProfileImage);
 router.get('/auth/settings/company/countries', getCountries);
