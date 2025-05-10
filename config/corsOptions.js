f25d4312274cc34efe06e84e95c44fa93e259c85
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2025-05-10 19:55:23 +0300

    add more headers for cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -13,5 +13,5 @@ export const corsOptions = {
   optionsSuccessStatus: 200,
   credentials: true,                 // Allow cookies to be sent with requests
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Content-Type', 'Authorization', 'X-Requested-With']
 };
