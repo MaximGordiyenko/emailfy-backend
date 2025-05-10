bc875746f788845195fcb96e0abcd2ee69e7a83d
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2025-05-10 19:43:32 +0300

    fix cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,13 +1,17 @@
 import { allowedOrigins } from './allowedOrigins.js';
 
-//https://expressjs.com/en/resources/middleware/cors.html
+// https://expressjs.com/en/resources/middleware/cors.html
 export const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
+      console.log(`Blocked by CORS: ${origin}`);
       callback(new Error('Not allowed by CORS'));
     }
   },
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  credentials: true,                 // Allow cookies to be sent with requests
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 };
