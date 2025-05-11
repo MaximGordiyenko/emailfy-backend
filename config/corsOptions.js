fe48e15b5e02e3847a1203554bac53560e5b0a1a
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2025-05-11 17:29:00 +0300

    fix probacly couses issue with host railway

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,17 +1,24 @@
 import { allowedOrigins } from './allowedOrigins.js';
 
 // https://expressjs.com/en/resources/middleware/cors.html
+// export const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       console.log(`Blocked by CORS: ${origin}`);
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   optionsSuccessStatus: 200,
+//   credentials: true,                 // Allow cookies to be sent with requests
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Content-Type', 'Authorization', 'X-Requested-With']
+// };
 export const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      console.log(`Blocked by CORS: ${origin}`);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200,
-  credentials: true,                 // Allow cookies to be sent with requests
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials', 'Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  optionsSuccessStatus: 200,
+  credentials: false,
 };
