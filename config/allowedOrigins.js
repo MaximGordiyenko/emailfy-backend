b81907ff2a21f6a4fd621782cd59346530799810
MaximGordiyenko <maxim.gordiyenko@gmail.com>
2025-05-12 18:53:56 +0300

    fix uppercase allowence CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 export const allowedOrigins = [
-  'https://MaximGordiyenko.github.io',
+  'https://maximgordiyenko.github.io',
   'https://maximgordiyenko.github.io/emailfy-frontend',
   'http://localhost:3000',
 ];
